f622c3c85e2c33b9197fb3749c152d3b6d3448e7
GRANDØMX <grandomx@gmail.com>
2022-06-07 10:46:58 +0700

    Create anti_afk.lua

---
+++ anti_afk.lua
@@ -0,0 +1,6 @@
+wait(3)
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+end)
